2ca281de5a384327e9234ee1b67a55027630fa77
BlackZan1 <kenshin.nakamura.01@mail.ru>
2020-01-07 13:47:20 +0300

    Updates

---
+++ precache-manifest.f2e345d1d2b9d19ad0e151eebb3f0142.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afad400969618000e759327e66b2de43",
+    "url": "/react-pokemon-game/index.html"
+  },
+  {
+    "revision": "ebae13a17055bc88be5d",
+    "url": "/react-pokemon-game/static/css/main.af9c3959.chunk.css"
+  },
+  {
+    "revision": "d6c534743c01bb8a3999",
+    "url": "/react-pokemon-game/static/js/2.247095c0.chunk.js"
+  },
+  {
+    "revision": "ebae13a17055bc88be5d",
+    "url": "/react-pokemon-game/static/js/main.76a35ae0.chunk.js"
+  },
+  {
+    "revision": "39efe945039d574f1e87",
+    "url": "/react-pokemon-game/static/js/runtime-main.d3e290f5.js"
+  },
+  {
+    "revision": "0859692a4ac5b96389ff50ab3902c116",
+    "url": "/react-pokemon-game/static/media/bg.0859692a.mp3"
+  },
+  {
+    "revision": "35e5ff56fd52d9817c111c05ac564a88",
+    "url": "/react-pokemon-game/static/media/cloud.35e5ff56.png"
+  },
+  {
+    "revision": "331a52f85834917ec838c906277654cc",
+    "url": "/react-pokemon-game/static/media/eat.331a52f8.mp3"
+  },
+  {
+    "revision": "9a44f820fc09fb6222e24b2f0882a4d7",
+    "url": "/react-pokemon-game/static/media/menu.9a44f820.mp3"
+  },
+  {
+    "revision": "7d46eadb0f3e65937b5b71d2396cc633",
+    "url": "/react-pokemon-game/static/media/tree.7d46eadb.png"
+  }
+]);
